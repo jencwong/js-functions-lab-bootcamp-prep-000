46109de73dd9f56456c720c393798aaf4f6777d3
Jen W <jsc525@gmail.com>
2019-05-21 03:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
+
+function happyHolidaysTo(string) {
+  return `Happy ${holiday}, {name}!`
+}
